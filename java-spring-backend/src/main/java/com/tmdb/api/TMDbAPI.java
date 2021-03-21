e184ab6ec55d6b1a2b73574dfd84766463207433
Kemosabe <joshuapearson1993gmail.com>
2021-03-21 16:45:11 +0000

    cleanup and rename main app

--- java-spring-backend/src/main/java/com/tmdb/api/TMDbAPI.java
+++ java-spring-backend/src/main/java/com/tmdb/api/TMDbAPI.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class TestApplication {
+public class TMDbAPI {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TestApplication.class, args);
+		SpringApplication.run(TMDbAPI.class, args);
 	}
 
 	public String hello() {
